1212ca3a04562bd2b6bcb338ac3e0cea0aad1beb
jubobs <jubobs-oss@protonmail.com>
2021-05-25 08:13:22 +0200

    Add some Playgrounds for variables and pointers

--- src/string_indexing.go
+++ src/string_indexing.go
@@ -8,16 +8,17 @@ var i = -42
 func main() {
 	s := "Hello, 世界"
 	fmt.Println("length:", len(s))
-
 	fmt.Print("bytes: ")
 	for i := 0; i < len(s); i++ {
 		fmt.Print(s[i], " ")
 	}
 	fmt.Println()
 
-	// indexing a string out of bounds causes a panic
-	// fmt.Println(s[i])
-	// fmt.Println(s[len(s)])
+	// fmt.Println(s[i]) // panic!
+	// fmt.Println(s[len(s)]) // panic!
+
+	// s[0] = 'h' // compilation error
+	// fmt.Println(&s[i]) // compilation error
 }
 
 // END OMIT
