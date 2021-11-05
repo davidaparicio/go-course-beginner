4461a2777e819c665c9eb190e780359e23f57ff6
jubobs <jubobs-oss@protonmail.com>
2021-11-05 21:12:22 +0100

    Revamp main.slide a bit

--- src/map_not_concurrency_safe.go
+++ src/map_not_concurrency_safe.go
@@ -7,21 +7,24 @@ import (
 
 // START OMIT
 func main() {
-	var visits uint
+	m := make(map[string]uint)
 	var wg sync.WaitGroup
-	const concurrency = 50
-	const n = 1000
+	const (
+		concurrency = 100
+		updates     = 100
+		username    = "jub0bs"
+	)
 	wg.Add(concurrency)
 	for i := 0; i < concurrency; i++ {
 		go func() {
 			defer wg.Done()
-			for j := 0; j < n; j++ {
-				visits++ // HL
+			for j := 0; j < updates; j++ {
+				m["jub0bs"]++ // HL
 			}
 		}()
 	}
 	wg.Wait()
-	fmt.Println("visits:", visits)
+	fmt.Println("count:", m[username])
 }
 
 // END OMIT
