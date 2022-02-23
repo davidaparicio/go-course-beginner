729cde05908aed31a4900bf55f734bdb7db55428
jubobs <jubobs-oss@protonmail.com>
2022-02-23 13:29:41 +0100

    Minor fixes

--- src/array_indexing.go
+++ src/array_indexing.go
@@ -7,11 +7,11 @@ var i = 3
 
 func main() {
 	roles := [...]string{"Viewer", "Editor", "Admin"}
-	// fmt.Println(roles[i]) // panic // HL
-	// fmt.Println(roles[3]) // compilation error // HL
 	fmt.Println(&roles[0]) // HL
 	fmt.Println(&roles[1])
 	fmt.Println(&roles[2])
+	// fmt.Println(roles[i]) // panic // HL
+	// fmt.Println(roles[3]) // compilation error // HL
 }
 
 // END OMIT
--- src/deadlock_example.go
+++ src/deadlock_example.go
@@ -7,7 +7,7 @@ import (
 // START OMIT
 func main() {
 	ch := make(chan int)
-	ch <- 1
+	ch <- 42
 	fmt.Println(<-ch)
 }
 
