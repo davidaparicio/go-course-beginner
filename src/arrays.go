cc8904a0a473b26dec6bd249b7155ea33f59c652
jubobs <jubobs-oss@protonmail.com>
2022-04-13 09:00:21 +0200

    Fix slices about slices

--- src/arrays.go
+++ src/arrays.go
@@ -8,10 +8,11 @@ func main() {
 	fmt.Printf("type name: %T\n", userIds)
 	l := len(userIds) // HL
 	fmt.Printf("length: %d\n", l)
-	roles := [...]string{"Viewer", "Editor", "Admin"} // HL
-	fmt.Println(roles == roles)                       // HL
-	var funcs [0]func()
-	fmt.Println(funcs)
-	// fmt.Println(funcs == funcs) // compilation error  // HL
+	roles := [...]string{ // HL
+		"Viewer",
+		"Editor",
+		"Admin", // this final comma is mandatory // HL
+	}
+	fmt.Println(roles)
 	// END OMIT
 }
