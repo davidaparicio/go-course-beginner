1f47cd4d842eb25067a8bc7c08df416f3da20459
jubobs <jubobs-oss@protonmail.com>
2022-04-15 12:20:43 +0200

    Minor fixes

--- src/uncancellable.go
+++ src/uncancellable.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		printInts()
-	}()
-	wg.Wait()
-}
-
-// START OMIT
-func printInts() {
-	for i := 0; ; i++ {
-		fmt.Println(i)
-		time.Sleep(200 * time.Millisecond)
+// START1 OMIT
+func uncancellable() {
+	for {
+		fmt.Println("Still alive...")
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
-// END OMIT
+// END1 OMIT
+
+// START2 OMIT
+func main() {
+	go uncancellable()
+	time.Sleep(10 * time.Second) // simulate more work
+}
+
+// END2 OMIT
