fc86434e528d91322b9431e2184bd0b0df7991d9
jub0bs <jcretel-infosec+github@protonmail.com>
2023-06-09 11:15:48 +0200

    Minor fixes

--- src/unkeyed_literals.go
+++ src/unkeyed_literals.go
@@ -2,18 +2,16 @@ package main
 
 import "fmt"
 
-// START1 OMIT
+// START OMIT
 type Location struct {
 	Lat  float64
 	Long float64
 }
 
-// END1 OMIT
-
 func main() {
-	// START2 OMIT
-	strasbourg := Location{48.5734, 7.7521}       // unkeyed literal (avoid!)
-	paris := Location{Lat: 48.8566, Long: 2.3522} // keyed literal
+	strasbourg := Location{48.5734, 7.7521}       // unkeyed literal: bad!
+	paris := Location{Lat: 48.8566, Long: 2.3522} // keyed literal: better
 	fmt.Println(strasbourg, paris)
-	// END2 OMIT
 }
+
+// END OMIT
