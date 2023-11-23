4254907dbdd8d8a05c82cc24e6c32a1ad60d7789
jub0bs <jcretel-infosec+github@protonmail.com>
2023-11-23 12:34:48 +0100

    refactor functions and error handling

--- src/function_nil.go
+++ src/function_nil.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-// START OMIT
 func main() {
-	var greetings func(string) string
-	fmt.Printf("%T\n", greetings)
-	// fmt.Println(greetings == greetings) // try uncommenting this
-	fmt.Println(greetings == nil)
-	// greetings("Julien") // try uncommenting this
-
-	greetings = strings.ToUpper
-	fmt.Println(greetings("Julien!"))
+	// START OMIT
+	var cleanup func(string) string
+	fmt.Println(cleanup == nil) // true
+	cleanup()                   // panic!
+	// END OMIT
 }
-
-// END OMIT
