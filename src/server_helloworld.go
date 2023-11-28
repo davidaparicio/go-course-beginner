46ae3ca009ebe7debad37d763e3ac91883d1f9cc
jub0bs <jcretel-infosec+github@protonmail.com>
2023-11-28 12:43:19 +0100

    keep fixing

--- src/server_helloworld.go
+++ src/server_helloworld.go
@@ -7,15 +7,15 @@ import (
 )
 
 // START OMIT
-func handleHello(w http.ResponseWriter, _ *http.Request) { // HL
-	fmt.Fprint(w, "Hello, world!\n")
-}
-
 func main() {
 	http.HandleFunc("GET /hello", handleHello)                // HL
 	if err := http.ListenAndServe(":8080", nil); err != nil { // HL
 		log.Fatal(err)
 	}
+}
+
+func handleHello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, "Hello, world!\n")
 }
 
 // END OMIT
